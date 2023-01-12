767feffa991aa87357667048cc4917ad66776d99
JD Singh <info@fermatsolutions.dev>
2023-01-12 15:37:58 -0600

    Added Digital Ocean config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use rust_microservice::configuration::get_configuration;
 use rust_microservice::startup;
 use rust_microservice::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
@@ -13,8 +12,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres");
+        .connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
